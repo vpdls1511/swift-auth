3ff35f1afaac53421db489c43f13eac3ed20f450
nGyu <vpdls1511@gmail.com>
2026-03-10 21:30:10 +0900

    git commit -m "feat: 회원가입 서비스 구현"

--- src/main/java/me/ngyu/swift/auth/domain/user/service/UserService.java
+++ src/main/java/me/ngyu/swift/auth/domain/user/service/UserService.java
@@ -1,11 +1,29 @@
 package me.ngyu.swift.auth.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
 import me.ngyu.swift.auth.domain.user.dto.UserRegisterRequest;
+import me.ngyu.swift.auth.domain.user.entity.User;
+import me.ngyu.swift.auth.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-  public void register(UserRegisterRequest request) {
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
+  public void register(UserRegisterRequest request) {
+    if (userRepository.existsByEmail(request.email())) {
+      throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+    }
+
+    User user = User.builder()
+      .email(request.email())
+      .password(passwordEncoder.encode(request.password()))
+      .name(request.name())
+      .build();
+
+    userRepository.save(user);
   }
 }
