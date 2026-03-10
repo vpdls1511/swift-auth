563019441fd40f40dcae344f2fa2c712394cf7d2
nGyu <vpdls1511@gmail.com>
2026-03-10 20:48:29 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/me/ngyu/swift/auth/SwiftAuthApplication.java
@@ -0,0 +1,13 @@
+package me.ngyu.swift.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwiftAuthApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/ngyu/swift/auth/SwiftAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ngyu.swift.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftAuthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
